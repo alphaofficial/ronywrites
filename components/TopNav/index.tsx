82b3f2c7914f1fd3487aaaa57582694f790c2e54
Albert Akrong <alphaxsalt@gmail.com>
2022-03-06 11:42:25 +0000

    chore: clean up

--- components/TopNav/index.tsx
+++ components/TopNav/index.tsx
@@ -6,20 +6,24 @@ interface NavLinks {
   href: string;
   title: string;
   icon?: any;
+  isExternal: boolean;
 }
 const navLinks: NavLinks[] = [
   {
     href: "/",
     title: "About",
+    isExternal: false,
   },
   {
     href: "/blog",
     title: "Blog",
+    isExternal: false,
   },
   {
     href: "https://twitter.com/NanaAkuaVee",
     title: "Twitter",
     icon: BsTwitter,
+    isExternal: true,
   },
 ];
 
@@ -27,9 +31,9 @@ const TopNav = () => {
   return (
     <Box mb="20px">
       <HStack spacing="20px">
-        {navLinks.map(({ href, icon, title }) => (
+        {navLinks.map(({ href, icon, title, isExternal }) => (
           <NextLink key={title} href={href} passHref>
-            <Link>
+            <Link isExternal={isExternal}>
               {icon ? (
                 icon({ size: 20, color: "orange" })
               ) : (
